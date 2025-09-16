c8a2aa51d15ec4d446401f2df580477f445ebefe
nishoof <nishilanand21@gmail.com>
2025-09-16 10:35:24 -0700

    replace `lab03` with `project02`

--- extract_test.go
+++ extract_test.go
@@ -12,8 +12,8 @@ func TestExtract(t *testing.T) {
 		testData, wantWords, wantHrefs []string
 	}{
 		{testData[1], []string{"hello", "cs", "272", "there", "are", "no", "links", "here"}, []string{}},
-		{testData[2], []string{"for", "a", "simple", "example", "see", "simple", "html"}, []string{"/test-data/lab03/simple.html"}},
-		{testData[3], []string{"style", "here", "is", "a", "blue", "link", "to", "href", "html", "and", "a", "red", "link", "to", "simple", "html"}, []string{"/test-data/lab03/href.html", "/test-data/lab03/simple.html"}},
+		{testData[2], []string{"for", "a", "simple", "example", "see", "simple", "html"}, []string{"/test-data/project02/simple.html"}},
+		{testData[3], []string{"style", "here", "is", "a", "blue", "link", "to", "href", "html", "and", "a", "red", "link", "to", "simple", "html"}, []string{"/test-data/project02/href.html", "/test-data/project02/simple.html"}},
 	}
 
 	for testIdx, test := range tests {

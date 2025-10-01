7a9d33f01b901f6229e003573a5997cdc87b24f0
nishoof <nishilanand21@gmail.com>
2025-09-30 17:28:35 -0700

    move search into its own file

--- index_interface.go
+++ index_interface.go
@@ -1,23 +1,15 @@
 package main
 
-type Result struct {
-	url         string
-	occurrences int
-	score       float32
-}
-
-type Results []Result
-
 type IndexType bool
 
 const IN_MEM IndexType = false
 const SQLITE IndexType = true
 
 type Index interface {
+	GetDocs() []string
 	GetFrequency(word, documentName string) int
 	GetNumDocs() int
 	GetNumDocsWithWord(word string) int
 	GetWordCount(documentName string) int
 	Increment(word, documentName string)
-	Search(word string) Results
 }

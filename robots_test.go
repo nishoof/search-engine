6790c80ea1918ee6aa14897a2ae3facf211dfd3b
nishoof <nishilanand21@gmail.com>
2025-10-07 08:27:58 -0700

    test: TestCrawlDelay and TestDisallow

---
+++ robots_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestCrawlDelay(t *testing.T) {
+	t1 := time.Now()
+	crawl("http://localhost:8080/top10/Dracula%20%7C%20Project%20Gutenberg/index.html")
+	t2 := time.Now()
+	if t2.Sub(t1) < (10 * time.Second) {
+		t.Errorf("TestDisallow was too fast\n")
+	}
+}
+
+func TestDisallow(t *testing.T) {
+	got := Search("blood", idx)
+	disallow := "chap21.html"
+
+	for _, result := range got {
+		if strings.Contains(result.url, disallow) {
+			t.Errorf("%s is in results when searching \"blood\"but it contains %s\n", result.url, disallow)
+		}
+	}
+}

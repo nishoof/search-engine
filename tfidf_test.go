21ded948d2a314dbb026595668617d0c82261c9c
nishoof <nishilanand21@gmail.com>
2025-10-16 16:16:01 -0700

    use top10 for tfidf_test test data

--- tfidf_test.go
+++ tfidf_test.go
@@ -8,19 +8,17 @@ func TestTfIdf(t *testing.T) {
 	tests := []struct {
 		word, doc              string
 		numWordsInDoc, numDocs int
-		index                  Index
 		want                   float64
 	}{
 		{
-			"simple", "index", 3, 1, IndexInMemory{
-				map[string]map[string]int{"simple": {"index": 1}},
-				map[string]int{"index": 3},
-			}, 0.16666666666666667,
+			"blood",
+			"http://localhost:8080/top10/Dracula%20%7C%20Project%20Gutenberg/chap10.html",
+			1663, 331, 0.016404221154672147,
 		},
 	}
 
 	for testIdx, test := range tests {
-		tfidf := tfidf(test.word, test.doc, test.numWordsInDoc, test.numDocs, test.index)
+		tfidf := tfidf(test.word, test.doc, test.numWordsInDoc, test.numDocs, idx)
 		if tfidf != test.want {
 			t.Errorf("test %d failed. got %f, want %f\n",
 				testIdx, tfidf, test.want)

a6f1831657160e7af0f7e3e269909d4438067082
nishoof <nishilanand21@gmail.com>
2025-10-17 14:40:44 -0700

    remove crawl return value
    crawl used to return a map that mapped page urls to the words found on
    that page, and this map was used to build the index. crawl was then
    refactored to build the index itself. so the map is no longer needed.
    I'll also be working on concurrency soon, which I think will make it
    much harder to build this map.

--- crawl_test.go
+++ crawl_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -9,22 +8,28 @@ func TestCrawl(t *testing.T) {
 	ts := getTestServer()
 	defer ts.Close()
 
+	seed := ts.URL + testPaths[0]
+	var idx Index = NewIndexInMemory()
+	crawl(seed, true, &idx)
 	tests := []struct {
-		seed string
-		want map[string][]string
+		word string
+		url  string
+		want int
 	}{
-		{testPaths[0], map[string][]string{
-			ts.URL + testPaths[0]: {"simple", "href", "style"},
-			ts.URL + testPaths[1]: {"272", "links"},
-			ts.URL + testPaths[2]: {"simple", "simple"},
-			ts.URL + testPaths[3]: {"style", "blue", "link", "href", "red", "link", "simple"},
-		}},
+		{"href", testPaths[0], 1},
+		{"272", testPaths[1], 1},
+		{"simpl", testPaths[2], 2}, // "simple" stems to "simpl"
+		{"style", testPaths[3], 1},
+		{"blue", testPaths[3], 1},
+		{"link", testPaths[3], 2},
+		{"red", testPaths[3], 1},
+		{"67", testPaths[0], 0},
 	}
 
 	for _, test := range tests {
-		got := crawl(ts.URL+test.seed, true, nil)
-		if !reflect.DeepEqual(got, test.want) {
-			t.Errorf("For seed %q, got %v but wanted %v\n", test.seed, got, test.want)
+		got := idx.GetFrequency(test.word, ts.URL+test.url)
+		if got != test.want {
+			t.Errorf("For word %q and url %q, got %d but wanted %d\n", test.word, test.url, got, test.want)
 		}
 	}
 }

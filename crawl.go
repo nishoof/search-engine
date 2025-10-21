e2620a2abc6a113363d3c9e1fa50925fb817b29a
nishoof <nishilanand21@gmail.com>
2025-10-21 11:03:19 -0700

    use wait groups to detect crawl completion

--- crawl.go
+++ crawl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -18,18 +19,21 @@ type Extracted struct {
 	words []string
 }
 
-func manager(urlCh chan string, downloadableUrlCh chan string, visitedSet map[string]struct{}, rules *RobotsRules, fastMode bool) {
+func manager(urlCh chan string, downloadableUrlCh chan string, visitedSet map[string]struct{}, rules *RobotsRules, fastMode bool, wg *sync.WaitGroup, inProgCh chan bool) {
 	fmt.Println("Manager started")
 
 	for url := range urlCh {
 		_, visited := visitedSet[url]
 		if visited {
+			wg.Done()
 			continue
 		}
 		visitedSet[url] = struct{}{}
 		if rules.Disallowed(url) {
+			wg.Done()
 			continue
 		}
+		inProgCh <- true
 		if !fastMode {
 			fmt.Printf("crawling %s\n", url)
 		}
@@ -50,22 +54,19 @@ func sleeper(requestCh chan bool, readyCh chan bool, rules *RobotsRules) {
 	fmt.Println("Sleeper ended")
 }
 
-func downloader(downloadableUrlCh chan string, bodyCh chan Downloaded, requestCh chan bool, readyCh chan bool, inProgCh chan bool) {
+func downloader(downloadableUrlCh chan string, bodyCh chan Downloaded, requestCh chan bool, readyCh chan bool) {
 	fmt.Println("Downloader started")
 
 	for url := range downloadableUrlCh {
 		<-readyCh
-		inProgCh <- true
 		// fmt.Printf("Downloader: Downloading %s\n", url)
 		body := download(url)
 		requestCh <- true
 
 		if body == nil {
-			<-inProgCh
 			continue
 		}
 		bodyCh <- Downloaded{url, body}
-		<-inProgCh
 	}
 
 	fmt.Println("Downloader ended")
@@ -97,7 +98,7 @@ func extractor(bodyCh chan Downloaded, pageCh chan Extracted, dirtyUrlCh chan st
 	fmt.Println("Extractor ended")
 }
 
-func builder(pageCh chan Extracted, idx *Index) {
+func builder(pageCh chan Extracted, idx *Index, wg *sync.WaitGroup, inProgCh chan bool) {
 	fmt.Println("Builder started")
 
 	for page := range pageCh {
@@ -109,42 +110,39 @@ func builder(pageCh chan Extracted, idx *Index) {
 		for _, word := range words {
 			(*idx).Increment(word, url)
 		}
+		wg.Done()
+		<-inProgCh
 	}
 
 	fmt.Println("Builder ended")
 }
 
-func adder(dirtyUrlCh chan string, urlCh chan string, host string) {
+func adder(dirtyUrlCh chan string, urlCh chan string, host string, wg *sync.WaitGroup, inProgCh chan bool) {
 	fmt.Println("Adder started")
 
 	for dirtyUrl := range dirtyUrlCh {
 		url := cleanHref(host, dirtyUrl)
 		if extractHost(url) == host {
 			urlCh <- url
+			wg.Add(1)
 		}
 	}
 
 	fmt.Println("Adder ended")
 }
 
-func blockUntilChannelsEmpty(ch1 chan string, ch2 chan string, ch3 chan Downloaded, ch4 chan Extracted, ch5 chan string, print bool) {
-	// time.Sleep(100 * time.Millisecond)
+func telemetry(telemetryDoneCh chan bool, visitedSet map[string]struct{}, ch1 chan string, ch2 chan string, ch3 chan Downloaded, ch4 chan Extracted, ch5 chan string, inProfCh chan bool) {
+	fmt.Println("Telemetry started")
+
 	for {
-		if print {
-			fmt.Printf("%d %d %d %d %d\n", len(ch1), len(ch2), len(ch3), len(ch4), len(ch5))
-		}
-		if len(ch1) == 0 && len(ch2) == 0 && len(ch3) == 0 && len(ch4) == 0 && len(ch5) == 0 {
+		fmt.Printf("TELEMETRY: %d visited, channels: %d %d %d %d %d. in progress: %d\n", len(visitedSet), len(ch1), len(ch2), len(ch3), len(ch4), len(ch5), len(inProfCh))
+		if len(telemetryDoneCh) > 0 {
 			break
 		}
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 	}
-}
 
-func telemetry(visitedSet map[string]struct{}, ch1 chan string, ch2 chan string, ch3 chan Downloaded, ch4 chan Extracted, ch5 chan string, inProfCh chan bool) {
-	for {
-		time.Sleep(10 * time.Millisecond)
-		fmt.Printf("\n%d visited, channels: %d %d %d %d %d. in progress downloads: %d", len(visitedSet), len(ch1), len(ch2), len(ch3), len(ch4), len(ch5), len(inProfCh))
-	}
+	fmt.Println("Telemetry ended")
 }
 
 /* Crawls the website starting from the given seed URL, then crawling all links found on that page, and so on for links found on those pages. fastMode ignores crawl-delay and prints less. If idx is not nil, crawl will also build the index using the index's Increment method */
@@ -166,36 +164,47 @@ func crawl(seed string, fastMode bool, idx *Index) {
 		rules.SetCrawlDelay(0)
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	telemetryDoneCh := make(chan bool, 1)
 	urlCh := make(chan string, 10000)
 	downloadableUrlCh := make(chan string, 100000)
 	requestCh := make(chan bool, 10000)
-	readyCh := make(chan bool, 10000)
+	readyCh := make(chan bool)
 	bodyCh := make(chan Downloaded, 10000)
 	pageCh := make(chan Extracted, 10000)
 	dirtyUrlCh := make(chan string, 10000)
-
 	inProgCh := make(chan bool, 100000)
 
-	// go telemetry(visitedSet, urlCh, downloadableUrlCh, bodyCh, pageCh, dirtyUrlCh, inProgCh)
-
-	go manager(urlCh, downloadableUrlCh, visitedSet, rules, fastMode)
+	go telemetry(telemetryDoneCh, visitedSet, urlCh, downloadableUrlCh, bodyCh, pageCh, dirtyUrlCh, inProgCh)
+	go manager(urlCh, downloadableUrlCh, visitedSet, rules, fastMode, &wg, inProgCh)
 	go sleeper(requestCh, readyCh, rules)
-	for i := 0; i < 10000; i++ {
-		go downloader(downloadableUrlCh, bodyCh, requestCh, readyCh, inProgCh)
+	for i := 0; i < 1000; i++ {
+		go downloader(downloadableUrlCh, bodyCh, requestCh, readyCh)
 	}
 	go extractor(bodyCh, pageCh, dirtyUrlCh)
-	go builder(pageCh, idx)
-	go adder(dirtyUrlCh, urlCh, host)
+	go builder(pageCh, idx, &wg, inProgCh)
+	go adder(dirtyUrlCh, urlCh, host, &wg, inProgCh)
 
 	urlCh <- seed
 	readyCh <- true
-	// blockUntilChannelsEmpty(urlCh, downloadableUrlCh, bodyCh, pageCh, dirtyUrlCh, true)
-	time.Sleep(2 * time.Second)
+	wg.Wait()
 
 	endTime := time.Now()
 	duration := endTime.Sub(startTime).Seconds()
 	numUrls := len(visitedSet)
 	fmt.Printf("crawled %d urls in %.2f seconds (%.2f per second)\n", numUrls, duration, (float64)(numUrls)/duration)
+
+	telemetryDoneCh <- true
+	close(telemetryDoneCh)
+	close(urlCh)
+	close(downloadableUrlCh)
+	close(requestCh)
+	close(bodyCh)
+	close(pageCh)
+	close(dirtyUrlCh)
+	close(inProgCh)
 }
 
 /* Extracts the host from the given href */
